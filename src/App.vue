2313beb56d67ba60c28c54d5529216a70fa94700
AngelClover <angelclover@aliyun.com>
2022-11-25 18:15:19 +1300

    add #jobs page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <template>
   <v-app>
     <v-main>
-      <List />
+      <component :is="currentView" />
     </v-main>
   </v-app>
 </template>
 
-<script setup>
+<script >
   import List from '@/components/list.vue'
+  import Jobs from '@/components/jobs.vue'
+export default {
+  data() {
+    return {
+      currentPath: window.location.hash
+    }
+  },
+  computed: {
+    currentView() {
+      console.log("Angel ", this.currentPath)
+      //pa=this.currentPath.slice(1)
+      //console.log("Angel pa:", this.currentPath.slice(1))
+      if (this.currentPath.slice(1) == "jobs") return Jobs
+      return List
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+      this.currentPath = window.location.hash
+      console.log("Angel ", this.currentPath, " in event listener")
+      console.log("Angel location:", window.location, " in event listener")
+    })
+  }
+}
 </script>
---
+++ src/components/jobs.vue
@@ -0,0 +1,69 @@
+<template>
+  
+  <v-container>
+    <div>
+      当前浏览器时间 : {{timecounter}}
+    </div>
+    this is jobs
+    <div>
+      ss1
+    </div>
+    <v-btn> ss1触发 </v-btn>
+    <div>
+      {{ss1}}
+    </div>
+
+    <div>
+      prefill
+    </div>
+    <v-btn> prefill触发 </v-btn>
+    <div>
+      {{prefill}}
+    </div>
+
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+          timecounter: null,
+            ss1: {},
+          prefill: {},
+        }
+    },
+      mounted() {
+          console.log("mounted")
+          console.log(process.env.NODE_ENV)
+          console.log(import.meta.env.VITE_BACKEND_ROOT)
+          this.getss1()
+          this.getprefill()
+          setInterval(() => {
+          this.getss1()
+          this.getprefill()
+            }, 10000)
+          setInterval(() => {
+              this.timecounter = new Date()
+            }, 1000)
+        },
+      methods: {
+          getss1() {
+              console.log("start get job_status_ss1")
+              this.axios.get(import.meta.env.VITE_BACKEND_ROOT + "db/job_status_ss1").then((res) => {
+                  console.log(res.data)
+                  this.ss1 = res.data
+                })
+            },
+          getprefill() {
+              console.log("start get job_status_prefill")
+              this.axios.get(import.meta.env.VITE_BACKEND_ROOT + "db/job_status_prefill").then((res) => {
+                  console.log(res.data)
+                  this.prefill = res.data
+                })
+            }
+
+        }
+
+  }
+</script>
